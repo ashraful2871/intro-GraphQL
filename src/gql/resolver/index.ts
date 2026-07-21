d1af240f16e4c093a084484c03bdc69e8ec8ef12
Md. Ashraful Islam <ashrafulislam2758@gmail.com>
2026-07-21 23:25:23 +0600

    cleanup code

--- src/gql/resolver/index.ts
+++ src/gql/resolver/index.ts
@@ -3,27 +3,37 @@ import { db } from "../../db.js";
 export const resolvers = {
   Query: {
     products: () => db.products,
-    product: (parent: any, args: { productId: string }, context: any) => {
-      const result = db.products.find((pd) => pd.id === args.productId);
+    product: (
+      parent: any,
+      { productId }: { productId: string },
+      context: any,
+    ) => {
+      const result = db.products.find((pd) => pd.id === productId);
       return result;
     },
     categories: () => db.categories,
-    category: (parent: any, args: { categoryId: string }, context: any) => {
-      const result = db.categories.find((ct) => ct.id === args.categoryId);
+    category: (
+      parent: any,
+      { categoryId }: { categoryId: string },
+      context: any,
+    ) => {
+      const result = db.categories.find((ct) => ct.id === categoryId);
       return result;
     },
   },
   Product: {
-    category: (parent: any, args: any, context: any) => {
+    category: (
+      { categoryId }: { categoryId: string },
+      args: any,
+      context: any,
+    ) => {
       //   console.log(parent.categoryId, args, context);
-      return db.categories.find(
-        (category) => category.id === parent.categoryId,
-      );
+      return db.categories.find((category) => category.id === categoryId);
     },
-    reviews: (parent: any, args: any, context: any) => {
+    reviews: ({ id }: { id: string }, args: any, context: any) => {
       //   console.log(parent);
 
-      return db.reviews.filter((review) => review.productId === parent.id);
+      return db.reviews.filter((review) => review.productId === id);
     },
   },
   Category: {
